87045bbf1a008c6a55a49703cfc3b4a6fda2795e
umamakhalid <umamakhalid39@gmail.com>
2021-11-29 23:41:50 +0500

    Initial commit

---
+++ app/src/main/java/com/umama/easypaisaexampleandroidapp/EasyPaisaAndroidSDK.java
@@ -0,0 +1,14 @@
+package com.umama.easypaisaexampleandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EasyPaisaAndroidSDK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy_paisa_android_sdk);
+    }
+}
---
+++ app/src/main/java/com/umama/easypaisaexampleandroidapp/EasyPaisaRestAPIs.java
@@ -0,0 +1,14 @@
+package com.umama.easypaisaexampleandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EasyPaisaRestAPIs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy_paisa_rest_apis);
+    }
+}
